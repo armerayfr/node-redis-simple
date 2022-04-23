1aca3882f7586f1f03d5a19c70a40b960630c443
armerayfr <armerray@gmail.com>
2022-04-23 13:28:13 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const axios = require("axios");
+const { createClient } = require("redis");
+const app = express();
+const port = 4000;
+
+const client = createClient();
+
+client.on("error", (err) => console.log("Redis Client Error", err));
+
+client.on("connect", () => console.log("connected to redis!"));
+
+app.get("/data/:searchtext", (req, res) => {
+  try {
+    const searchtext = req.params.searchtext;
+    client.get(searchtext, async (err, data) => {
+      if (data) {
+        return res.status(200).send({
+          error: false,
+          message: `Data for ${searchtext} from the cache`,
+          data: JSON.parse(data),
+        });
+      } else {
+        const recipe = await axios.get(
+          `https://jsonplaceholder.typicode.com/${searchtext}`
+        );
+        client.setex(searchtext, 1020, JSON.stringify(recipe.data));
+        return res.status(200).send({
+          error: false,
+          message: `Data for ${searchtext} from the server`,
+          data: recipe.data,
+        });
+      }
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
